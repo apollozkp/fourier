56ebf1bc4616e3401da80e0ffced10d9dcd59b1c
Rubenduburck <rubenduburck@gmail.com>
2024-06-11 09:51:12 +0000

    switch encoding to base64

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,5 @@ pub fn timed<T>(name: &str, f: impl FnOnce() -> T) -> T {
     tracing::debug!("{} took {:?}", name, elapsed);
     result
 }
+
+pub const B64ENGINE: base64::engine::GeneralPurpose = base64::engine::general_purpose::STANDARD;
